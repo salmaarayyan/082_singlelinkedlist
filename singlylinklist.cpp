84e94ce0fcd7e9c2e0397d76c9e01669bd863b2c
salmaarayyan <salmaarayyan@gmail.com>
2024-04-26 15:42:45 +0800

    deklarasi pernyataan previous dimulai

--- singlylinklist.cpp
+++ singlylinklist.cpp
@@ -13,4 +13,24 @@ void addNode() {
     int nim;
     string nama;
     Node* nodeBaru = new Node();
+    cout << "Masukan NIM : ";
+    cin >> nim;
+    cout << "Masukan Nama : ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+
+    if (START == NULL || nim <= START->noMhs) {
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+    }
+
+Node* previous = START;
 }
