15a18d100cd5547bdf82eef8883fa71718a5e25f
salmaarayyan <salmaarayyan@gmail.com>
2024-04-26 15:46:43 +0800

    deklarasi perintah untuk mencari sebuah node dalam link list

--- singlylinklist.cpp
+++ singlylinklist.cpp
@@ -34,4 +34,18 @@ void addNode() {
 
 Node* previous = START;
 Node* current = START;
+
+while ((current != NULL) && (nim >= current->noMhs))
+{
+    if (nim == current->noMhs)
+    {
+        cout << "NIM sudah ada" << endl;
+        return;
+    }
+    previous = current;
+    current = current->next;
+}
+
+nodeBaru->next = current;
+previous->next = nodeBaru;
 }
